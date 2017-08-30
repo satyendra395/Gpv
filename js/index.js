f82cb208e4adf0ed3492abd9e097dbbd446bb8f8
sanjay04121991 <spandey@webcontentor.com>
2017-08-30 11:29:43 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import{BrowserRouter as Router, 
+    Route,
+    Link,
+    Switch
+} from 'react-router-dom';
+
+const app = document.getElementById('app');
+ReactDOM.render(<h1>React first app </h1>, app)
